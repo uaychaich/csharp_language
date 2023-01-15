53cbc1fd2bb5c24dacc7b12769c3b35ac5f50c09
Uaychai Chotjaratwanich <13571179+uaychaich@users.noreply.github.com>
2023-01-15 00:46:59 +0000

    2023-01-15

---
+++ 27_fcc9_collections/solution/SampleCollection/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+    Author: Uaychai Chotjaratwanich
+    Modified Date: 2023-01-15
+*/
+using System.Collections;
+
+int[] a1 = new int[2]; a1[0] = 5; a1[1] = 10; 
+foreach (var item in a1)
+{   Console.WriteLine(item);}
+
+ArrayList b1 = new ArrayList();
+b1.Add(5); b1.Add("Uaychai");
