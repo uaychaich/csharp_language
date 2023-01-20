c01dac2197aff807bf4362893844e6601011fef8
Uaychai Chotjaratwanich <uchotjaratwanich@gmail.com>
2023-01-20 14:32:51 +0700

    2023-01-20

--- 35_a8e4_check_data_app/solution/SampleCheckDataApp/SampleStructures.cs
+++ 35_a8e4_check_data_app/solution/SampleCheckDataApp/SampleStructures.cs
@@ -6,6 +6,6 @@ public struct PersonStructure
 {
     [StringNotEmply(3,ErrorMessage = "FirstName must have 3 character")]
     public string FirstName{get;set;}
-    [StringNotEmply(4,ErrorMessage = "FirstName must have 4 character")]
+    [StringNotEmply(4,ErrorMessage = "LastName must have 4 character")]
     public string LastName{get;set;}
 }
