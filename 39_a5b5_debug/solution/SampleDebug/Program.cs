d03df7a917b57911d6055c4a2a218ae2ddcc44b5
Uaychai Chotjaratwanich <uchotjaratwanich@gmail.com>
2023-01-27 13:54:26 +0700

    2023-01-27

--- 39_a5b5_debug/solution/SampleDebug/Program.cs
+++ 39_a5b5_debug/solution/SampleDebug/Program.cs
@@ -1,9 +1,17 @@
 ﻿/*
     Author: Uaychai Chotjaratwanich
-    Modified Date: 2023-01-26
+    Modified Date: 2023-01-27
 */
 using System.Diagnostics;
 
 Console.WriteLine("Uaychai is here");
 int a = 5; Debug.WriteLine($"Debug a:{a}");
 Console.WriteLine(a);
+Debug.WriteLine("Begin Loop");Debug.Indent();
+for(a=0;a<5;a++)
+{
+    Console.WriteLine(a);
+    Debug.WriteLineIf(a==2,$"Debug a:{a}");
+}
+Debug.Unindent();
+Debug.WriteLine("End Loop");
